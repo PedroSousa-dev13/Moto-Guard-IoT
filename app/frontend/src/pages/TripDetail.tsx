2a3570ae44e10e03af5c06c64711c4bfbf9aae1e
Pedro Sousa <al78656@alunos.utad.pt>
2026-04-26 23:46:41 +0100

    fixed

--- app/frontend/src/pages/TripDetail.tsx
+++ app/frontend/src/pages/TripDetail.tsx
@@ -48,12 +48,14 @@ L.Icon.Default.mergeOptions({
 });
 
 function formatDateTime(date: string) {
-  return new Date(date).toLocaleString("pt-PT", {
+  const d = new Date(date);
+  return d.toLocaleString("pt-PT", {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
+    second: "2-digit",
   });
 }
 
